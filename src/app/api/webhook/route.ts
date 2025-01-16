e1649d1830bbfbe807fc5dfb467605fda95f1a00
Asmaa Mohamed <33634459+AsmaaaMohamed@users.noreply.github.com>
2025-01-16 17:31:03 +0200

    comit

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -9,6 +9,32 @@ export async function POST(req : Request){
     const signature = (await headers()).get("Stripe-Signature") as string;
     let event: Stripe.Event;
     try{
-        
+        event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET!
+        )
+    }
+    catch(error:any){
+        return new NextResponse(`Webhook Error: ${error.message}`, {
+            status:400
+        });
+    }
+    const session = event.data.object as Stripe.Checkout.Session;
+    const userId = session?.metadata?.userId;
+    const courseId = session?.metadata?.courseId;
+    if(event.type === "checkout.session.completed"){
+        if(!userId || !courseId){
+            return new NextResponse("Webhook Error: Missing metadat" , { status:400});
+        }
+        await prisma.usersCourses.create({
+            data:{
+                courseId: courseId,
+                userId: userId
+            }
+        })
+    }
+    else{
+        return new NextResponse(`Webhook Error: Unhandled event type ${event.type}`, {status:200})
     }
 }
