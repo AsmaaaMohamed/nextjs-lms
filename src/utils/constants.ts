33653ae033a8e978c1d581215148ac5e19b88b71
Asmaa Mohamed <33634459+AsmaaaMohamed@users.noreply.github.com>
2025-02-09 14:52:14 +0200

    comit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const POSTS_PER_PAGE = 6;
 
-const PRODUCTION_DOMAIN = "https://nextjs-lms-two.vercel.app";
+const PRODUCTION_DOMAIN =
+  "https://nextjs-lms-git-master-asmaaamohameds-projects.vercel.app";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 
 export const DOMAIN =
